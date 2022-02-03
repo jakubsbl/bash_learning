e92aa82ea5f84ace8b71f3accd17f09be6802d38
jakubsbl <jakbusobol96@gmail.com>
2022-02-03 20:23:07 +0100

    zmiana skryptu

--- operations.sh
+++ operations.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
-#stworzylem katalog "lubie"
-mkdir lubie
-readlink -f lubie > zawartosc.txt
-mv zawartosc.txt lubie
 
+mkdir lubie
+cd lubie
+readlink -f . > zawartosc.txt
+mkdir kawke
+cd kawke
+readlink -f . > zawartosc.txt
+mkdir zbozowa
+cd zbozowa
+readlink -f . > zawartosc.txt
+cd ..
+cd ..
+mkdir placki
+cd placki
+readlink -f . > zawartosc.txt
+cd ..
+mkdir herbate
+cd herbate
+readlink -f . > zawartosc.txt
+mkdir ziolowa
+cd ziolowa
+readlink -f . > zawartosc.txt
 
