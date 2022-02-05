a365a004ff095a03fb5f6c3194cc395838742751
jakubsbl <jakubsobol96@gmail.com>
2022-02-05 01:00:14 +0100

    wojewodztwa

---
+++ cwiczenia/wojewodztwa.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+
+mkdir dolnoslaskie
+mkdir kujawsko-pomorskie
+mkdir lubelskie
+mkdir lubuskie
+mkdir lodzkie
+mkdir malopolskie
+mkdir mazowieckie
+mkdir opolskie
+mkdir podkarpackie
+mkdir podlaskie
+mkdir pomorskie
+mkdir slaskie
+mkdir swietokrzyskie
+mkdir warminsko-mazurskie
+mkdir wielkopolskie
+mkdir zachodniopomorskie
+cd dolnoslaskie 
+echo Wroclaw > stolica.txt
+cd ..
+cd kujawsko-pomorskie
+echo Bydgoszcz> stolica.txt
+cd ..
+cd lubelskie 
+echo Lublin > stolica.txt
+cd ..
+cd lubuskie 
+echo Gorzow Wielkopolski > stolica.txt 
+cd ..
+cd malopolskie
+echo Krakow > stolica.txt
+cd ..
+cd mazowieckie
+echo Warszawa > stolica.txt
+cd ..
+cd opolskie 
+echo Opole > stolica.txt
+cd ..
+cd podkarpackie
+echo Rzeszow > stolica.txt
+cd ..
+cd podlaskie
+echo Bialystok > stolica.txt
+cd ..
+cd pomorskie
+echo Gdansk > stolica.txt
+cd .. 
+cd slaskie
+echo Katowice > stolica.txt
+cd ..
+cd swietokrzyskie
+echo Kielce > stolica.txt
+cd ..
+cd warminsko-mazurskie 
+echo Olsztyn > stolica.txt
+cd ..
+cd wielkopolskie
+echo Poznan > stolica.txt
+cd ..
+cd zachodniopomorskie
+echo Szczecin > stolica.txt
+
+echo OTO STOLICE WSZYSTKICH 16 WOJEWODZTW POLSKI.
+
+
+
+
+
+
