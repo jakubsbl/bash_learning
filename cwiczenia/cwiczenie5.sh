a091b8532f6926fe4572288f740aecec0137ad9c
jakubsbl <jakubsobol96@gmail.com>
2022-02-23 15:20:42 +0100

    dodatnie cwiczenia 5

---
+++ cwiczenia/cwiczenie5.sh
@@ -0,0 +1,30 @@
+
+
+#!/bin/bash
+ 
+#if $1 ma wartosc --help
+#echo wypisuje metode dzialania skryptu
+#fi
+
+help="Skrypt wyswietla podane argumenty w odwrotnej kolejnosci"
+
+
+if [[ $1 == "--help" ]];then
+	echo $help
+	exit 0
+fi
+
+
+#for i in $(ls)
+#do
+#	if [[ -f $i ]]; then
+#	echo "plik $i"
+#fi;
+#done;
+
+
+for i in $*
+do
+	echo "$i"
+	tac
+done
