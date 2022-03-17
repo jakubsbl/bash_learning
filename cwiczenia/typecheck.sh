6e0386d8f1d81b90a952c8c697f15be446aa2df6
jakubsbl <jakubsobol96@gmail.com>
2022-03-17 23:40:41 +0100

    i add script which tell what type of file is in argument no.1

---
+++ cwiczenia/typecheck.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Skrypt bedzie sprawdzal typ plikow, ktore podasz jako parametr (lokalizacja pliku) 
+
+# *katalog
+# *plik
+# *nie istnieje 
+
+if [[ $1 == "--help" ]];then
+    echo "Skrypt typechech.sh mówi o tym jakiego typu jest plik, ktory podasz jako parametr lub informuje, ze plik nie istnieje.
+    np. ./typecheck.sh plik123.txt
+    plik123.txt jest katalogiem"
+    exit 0
+fi
+
+if [[ -e $1 ]] && [[ -d $1 ]];then
+    echo $1 jest katalogiem
+elif [[ -e $1 ]] && [[ -f $1 ]];then
+    echo $1 jest plikiem
+else
+    echo $1 nie istnieje
+fi
